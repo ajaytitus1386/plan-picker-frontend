71e717f30c6db46b51e1200c5b09d6d86bf6bd5c
Ajay Titus <ajaytitus1386@gmail.com>
2022-09-07 02:42:07 +0530

    feat: add tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+
+const withMT = require("@material-tailwind/react/utils/withMT");
+
+module.exports = withMT({
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        lucidean: "#1e4d91",
+      },
+    },
+  },
+  plugins: [],
+});
