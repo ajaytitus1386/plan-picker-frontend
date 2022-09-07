4375d1d3d8e9d9d8556a4faea053fc9024a28739
Ajay Titus <ajaytitus1386@gmail.com>
2022-09-07 18:28:34 +0530

    fix: typo in url

--- utils/api.ts
+++ utils/api.ts
@@ -1,4 +1,4 @@
-const BASE_URL = "https://richpanel-task-backend.herokuapp.com/";
+const BASE_URL = "https://richpanel-task-backend.herokuapp.com";
 
 const AUTH_URL = `${BASE_URL}/user`;
 const PLAN_URL = `${BASE_URL}/plans`;
