570ee784d813a7212c5f8a8b944ba43bd3add37b
Ajay Titus <ajaytitus1386@gmail.com>
2022-09-07 17:02:53 +0530

    fix: add stripe price ids and routes

--- utils/api.ts
+++ utils/api.ts
@@ -3,6 +3,7 @@ const BASE_URL = "http://localhost:4000";
 const AUTH_URL = `${BASE_URL}/user`;
 const PLAN_URL = `${BASE_URL}/plans`;
 const SUB_URL = `${BASE_URL}/subscriptions`;
+const STRIPE_URL = `${BASE_URL}/stripe`;
 
 export const ROUTES = {
   AUTH: {
@@ -12,11 +13,14 @@ export const ROUTES = {
   },
   PLAN: {
     GET_ALL: `${PLAN_URL}/fetch`,
-    GET_ONE: (id: string) => `${AUTH_URL}/fetch/${id}`,
+    GET_ONE: (id: string) => `${PLAN_URL}/fetch/${id}`,
   },
   SUBSCRIPTION: {
     CREATE: `${SUB_URL}/create`,
     GET_BY_ID: `${SUB_URL}/fetch`, // user Id in query
     CANCEL: (subId: string) => `${SUB_URL}/cancel/${subId}`,
+  },
+  STRIPE: {
+    CREATE: `${STRIPE_URL}/create`,
   },
 };
---
+++ utils/stripePlans.ts
@@ -0,0 +1,18 @@
+export const stripePlans: Record<string, any> = {
+  basic: {
+    monthly: "price_1LfKNuSIV5sOInTH0FWgnSUV",
+    yearly: "price_1LfKNuSIV5sOInTH4j3hDP1e",
+  },
+  standard: {
+    monthly: "price_1LfKOWSIV5sOInTHlcqTL09L",
+    yearly: "price_1LfKOWSIV5sOInTHQklgsZYq",
+  },
+  premium: {
+    monthly: "price_1LfKP3SIV5sOInTHkEl49pTO",
+    yearly: "price_1LfKP3SIV5sOInTHR7cpKbsE",
+  },
+  regular: {
+    monthly: "price_1LfKPQSIV5sOInTHRqyWmkZM",
+    yearly: "price_1LfKPQSIV5sOInTHgc76oVHD",
+  },
+};
