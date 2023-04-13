252cbace458a3d71c28790910ba51092d2e77283
Ajay Titus <ajaytitus1386@gmail.com>
2023-04-13 17:35:42 +0530

    feat: add colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,23 +11,28 @@ module.exports = withMT({
     extend: {
       colors: {
         lucidean: "#1e4d91",
-        cornell: '#A8201A',
-        rose: '#B9314F',
-        cordovan: '#8C2F39',
-        'old-rose': '#CC7E85',
+        cornell: "#A8201A",
+        rose: "#B9314F",
+        cordovan: "#8C2F39",
+        "old-rose": "#CC7E85",
+        seasalt: "#FAFAFA",
+        jet: "#020100",
       },
       animation: {
-        'gradient-animate': 'bg-animate 45s ease infinite'
+        "gradient-animate": "bg-animate 45s ease infinite",
       },
       keyframes: {
-        'bg-animate': {
-          '0%, 100%': { 'background-position': '0% 50%' },
-          '50%': { 'background-position': '100% 50%' }
-        }
+        "bg-animate": {
+          "0%, 100%": { "background-position": "0% 50%" },
+          "50%": { "background-position": "100% 50%" },
+        },
       },
       backgroundSize: {
-        "animated": "400% 400%"
-      }
+        animated: "400% 400%",
+      },
+      backgroundImage: {
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+      },
     },
   },
   plugins: [],
